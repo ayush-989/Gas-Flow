b5c8c4c04735a2c1db2852385182ec26bae9c001
ayush-989 <ayusht9189@gmail.com>
2026-03-29 21:17:58 +0530

    fix: api config and vercel deployment

--- src/services/rapidMaps.js
+++ src/services/rapidMaps.js
@@ -18,8 +18,9 @@ export const getGoogleMapKey = async () => {
 export const searchLocation = async (query) => {
     try {
         const key = await getGoogleMapKey();
+        const mapApiUrl = import.meta.env.VITE_GOOGLE_MAPS_API_URL || 'https://maps.googleapis.com/maps/api';
         const response = await axios.get(
-            `https://maps.googleapis.com/maps/api/geocode/json`,
+            `${mapApiUrl}/geocode/json`,
             {
                 params: {
                     address: query,
